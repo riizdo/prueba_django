f48a655fa256964b4c4360c249fecbd0f132fcbd
root <ricardoizquierdopro@gmail.com>
2019-05-03 01:23:16 +0200

    desarrollando web

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,6 +5,8 @@ class Question(models.Model):
     question_text = models.CharField(max_length = 200)
     pub_date = models.DateTimeField('date publish')
     
+    def __str__(self):
+        return self.question_text
     
     
 class Choice(models.Model):
@@ -12,3 +14,5 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length = 200)
     votes = models.IntegerField(default = 0)
     
+    def __str__(self):
+        return self.choice_text
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,23 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hola Mundo!!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_cuestion_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "Son las respuestas de las preguntas %s"
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    response = "Vota las preguntas %s"
+    return HttpResponse(response % question_id)
